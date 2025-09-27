6e3abc7e6f0dfe6cad152b4836270594d58d0115
MudrykD5 <bonestone204@gmail.com>
2025-09-27 13:37:47 +0300

    Update ira_reminder.py

--- ira_reminder.py
+++ ira_reminder.py
@@ -78,7 +78,7 @@ def check_insurance(page, reg_number: str) -> dict:
         print('🥂radio button found')
         try:
             page.locator("div.cdk-overlay-container").evaluate("el => el.style.display='none'")
-             print('🥂overlay hidden)
+            
         except Exception as e:
             print(f'overlay hidden failed {e}')
         page.click("label[for='mat-radio-3-input']")
